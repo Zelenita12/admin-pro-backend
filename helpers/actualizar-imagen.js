c11d4572d2fd82f60bbe16424ade83ca6f359dd8
Elena Garcia <zel_elena@hotmail.com>
2023-10-09 22:25:22 +0200

    Fin seccion 11

---
+++ helpers/actualizar-imagen.js
@@ -0,0 +1,64 @@
+const fs = require('fs');
+
+const Usuario = require('../models/usuario');
+const Medico = require('../models/medico');
+const Hospital = require('../models/hospital');
+
+const borrarImagen = (path) => {
+    if (fs.existsSync(path)) {
+        fs.unlinkSync(path);
+    }
+};
+
+const actualizarImagen = async (tipo, id, nombreArchivo) => {
+    let pathViejo = '';  // Declara pathViejo una vez aquí
+
+    switch (tipo) {
+        case 'medicos':
+            const medico = await Medico.findById(id);
+            if (!medico) {
+                console.log('No es un médico por id');
+                return false;
+            }
+            pathViejo = `./uploads/medicos/${medico.img}`;
+            borrarImagen(pathViejo);
+
+            medico.img = nombreArchivo;
+            await medico.save();
+            return true;
+
+        case 'hospitales':
+            const hospital = await Hospital.findById(id);
+            if (!hospital) {
+                console.log('No es un hospital por id');
+                return false;
+            }
+            pathViejo = `./uploads/hospitales/${hospital.img}`;
+            borrarImagen(pathViejo);
+
+            hospital.img = nombreArchivo;
+            await hospital.save();
+            return true;
+
+        case 'usuarios':
+            const usuario = await Usuario.findById(id);
+            if (!usuario) {
+                console.log('No es un usuario por id');
+                return false;
+            }
+            pathViejo = `./uploads/usuarios/${usuario.img}`;
+            borrarImagen(pathViejo);
+
+            usuario.img = nombreArchivo;
+            await usuario.save();
+            return true;
+
+        default:
+            console.log('Tipo no reconocido');
+            return false;
+    }
+};
+
+module.exports = {
+    actualizarImagen
+};
